da7b958d865efdd95295f19d7f4e4fd91c0a490c
mak119 <mayank.uiet7@gmail.com>
2019-06-21 01:05:12 +0530

    setup mongoose cloud db path

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 'use strict';
 
 const express = require('exoress');
+
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const mongoose = require('mongoose');
 const app = express();
+
+const userRoutes = require('./routes/user');
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(userRoutes);
+
+mongoose.connect(`mongodb+srv://${mongodbUser}:${mongodbPassword}root@node-mak-btumf.mongodb.net/test?retryWrites=true&w=majority`,
+{
+    useMongoClient: true
+});
 
 const port = node.env.PORT || 3000;
 
