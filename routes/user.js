d7f6fc996c18e1ba22646fbbd074a4022b45e5d9
mak119 <mayank.uiet7@gmail.com>
2019-06-21 01:24:18 +0530

    added route for signup

--- routes/user.js
+++ routes/user.js
@@ -15,5 +15,35 @@ const emailUtils = require('../utils/133-email-utils');
 const jwt = require('jsonwebtoken');
 const SECRET_KEY = 'secretKey';
 
+// model
+const User = require('../models/user');
+const mongoose = require('mongoose');
 // to handle cross-origin-requests
 router.use(cors());
+
+router.post('/signup', (req, res, next)=>{
+    const body = req.body;
+    const otp = authUtils.generateOTP();
+    const data = {
+        _id: new mongoose.Types.ObjectId(),
+        firstName: body.firstName.toString(),
+        lastName: body.lastName ? body.lastName.toString():null,
+        phoneNumber: body.phoneNumber.toString(),
+        userCode: body.userCode.toString(),
+        email: email.userCode.toString(),
+        password: authUtils.hashPassword(req.body.password).toString(),
+        otp: otp,
+        isActive: true
+    };
+
+    const user = new User(data);
+    user.save()
+    .then((user, err)=>{
+        if(err) {
+            res.status(401).send('User Not Created');
+        }
+        res.status(200).send('User Created successfully');
+    });
+})
+
+module.exports = router;
