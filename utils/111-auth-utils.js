2b5086b9ae15312c87b6ed60552e43f140b4fcb5
mak119 <mayank.uiet7@gmail.com>
2019-06-21 00:53:02 +0530

    added auth-utils

---
+++ utils/111-auth-utils.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const app = require('../server');
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
+const salt = bcrypt.genSaltSync(saltRounds);
+
+module.exports = {
+    hashPassword: function (password) {
+    return bcrypt.hashSync(password, salt);
+  },
+
+  comparePassword: function (password, hashedPassword) {
+    password = '' + password; // to handle numeric passwords
+    hashedPassword = '' + hashedPassword;
+    return bcrypt.compareSync(password, hashedPassword);
+  },
+
+  generateOTP: function () { 
+    let OTP = ''; 
+    for (let i = 0; i < 6; i++ ) { 
+        OTP += Math.floor(Math.random() * 10); 
+    } 
+    return OTP; 
+    },
+    
+    verifyOTP: function(enteredOTP, dbOTP) {
+        if(enteredOTP === dbOTP) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
